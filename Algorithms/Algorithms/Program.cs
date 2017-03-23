fadfdf56e3cfa579e023774d19537108a0360469
Ajit <codemasterblackperl@outlook.com>
2017-03-23 10:46:56 +0530

    adding quick sort

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Algorithms.Sorting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
+            var arr = new int[]{ 2, 1, 3, 1, 2 };
+            ISort sort = new InsertionSort();
+            arr = sort.Sort(arr);
         }
     }
 }
---
+++ Algorithms/Algorithms/Sorting/InsertionSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sorting
+{
+    class InsertionSort:ISort
+    {
+        public int NoOfSteps { get; set; }
+
+        public int[] Sort(int[] arr)
+        {
+            var j = 0;
+            NoOfSteps = 0;
+            for (var i = 1; i < arr.Length; i++)
+            {
+                var value = arr[i];
+                j = i;
+                
+                while (j > 0 && arr[j - 1] > value)
+                {
+                    NoOfSteps++;
+                    arr[j] = arr[j - 1];
+                    j = j - 1;
+                }
+                arr[j] = value;
+            }
+            return arr;
+        }
+    }
+}
